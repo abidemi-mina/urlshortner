f0d9d4ac4bdabc2a2bd02317764dea9f6261eef4
abidemi-mina <aminatabidemi212@gmail.com>
2024-01-03 14:48:13 +0100

    'first-commit'

---
+++ shortner/views.py
@@ -0,0 +1,18 @@
+import uuid
+from django.shortcuts import render,redirect,HttpResponse
+from .models import *
+# Create your views here.
+def home(request):
+    return render ( request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link= link , unid= uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+def go(request,pk):
+    url_details = Url.objects.get(unid=pk)
+    return redirect('https://'+url_details.link)
